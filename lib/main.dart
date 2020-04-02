2f6376dbd0c88a9f061ca13b284fe9d32ca52a2e
ishihara <r.funk.7188@gmail.com>
2020-04-02 10:50:45 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFEEEEEE),
+
+      // Header
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Color(0xFFEEEEEE),
+        brightness: Brightness.light,
+        centerTitle: true,
+        title: Text(
+          'Instagram',
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(
+            Feather.camera,
+            color: Colors.black,
+          ),
+        ),
+        actions: <Widget>[
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Feather.tv,
+              color: Colors.black,
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              FontAwesome.send_o,
+              color: Colors.black,
+            ),
+          )
+        ],
+      ),
+
+      // Body
+      body: Container(
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Divider(),
+              Row(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
